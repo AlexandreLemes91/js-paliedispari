9c080dc31c2100cb56394fac1a065451a4952b2f
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-03-29 17:16:30 +0200

    pari e dispari

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+
+
+
+/**
+ * chiedere all'utente se desidera pari o dispari e un numero
+ * generare un numero per il bot
+ * somma dei 2 numeri
+ * stabilie se somma è pari o dispari
+ * stampa a video
+ */
+
+/* var userChoice = prompt( "Vuoi pari o dispari?" ).toLowerCase();
+while(( userChoice != "pari" ) && ( userChoice != "dispari" )){
+    userChoice = prompt( "Prego, inserire o pari o dispari" ).toLowerCase();
+}
+console.log(userChoice);
+
+var userNum = parseInt( prompt( "Inserire un numero da 1 a 5" ));
+while( isNaN( userNum ) || ( userNum < 1 ) || ( userNum > 5 ) ){
+    userNum = parseInt(prompt( "Valore non valido! Prego inserire un numero da 1 a 5" ));
+}
+console.log(userNum);
+
+var botNum = Math.floor( Math.random()*5 ) +1;
+console.log(botNum);
+
+var risultato = pariDispari(userNum, botNum);
+console.log(risultato);
+
+if( userChoice === risultato ){
+    console.log("HAI VINTO!!!")
+}else{
+    console.log("Mi dispiace... hai perso...")
+}
+
+
+function pariDispari (num1, num2){
+    var somma = num1 + num2;
+
+    if( somma % 2 == 0) {
+        var pariWin = "pari";
+        return pariWin
+    }else{
+        var pariLose = "dispari";
+        return pariLose
+    }
+} */
